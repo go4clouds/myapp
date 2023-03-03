a9f611cdbdbdce0dd573c673fee6308412ebd8c2
Michal Jura <mjura@suse.com>
2023-03-03 20:17:37 +0100

    Update myapp to v2.0

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
+import os
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from MyAPP v1.0!"
+    message=os.getenv('MYAPP_MESSAGE', "Hello from MyAPP v2.0!")
+    return message
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8081)
+    app.run(host='0.0.0.0', port=os.getenv('MYAPP_PORT', '8081'))
